663043d279ba892141dd3dec9d4f54ba9cfb266c
asam1234 <albertsam87@hotmail.com>
2023-01-10 23:53:25 +0000

    Add files via upload

---
+++ Project 2 - ML model - Visualisation of decision tree .py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+# Visualizing a Decision tree
+
+# we will export our model in a visual format to see how the model makes predictions 
+
+import pandas as pd 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree # object has a method for exporting decisiion tree in a graphical format 
+
+#import dataset
+sd = pd.read_csv('sportsdata.csv')
+
+# create input and output data set 
+X = sd.drop(columns = ['sport'])
+y = sd['sport']
+
+# create model 
+model = DecisionTreeClassifier()
+
+# train model 
+model.fit(X,y)
+
+tree.export_graphviz(model, out_file = 'sports_predictor.dot', 
+                     feature_names = ['age', 'gender'], 
+                     class_names = sorted(y.unique()),
+                     label = 'all',
+                     rounded=True, 
+                     filled=True)
+                     
+
