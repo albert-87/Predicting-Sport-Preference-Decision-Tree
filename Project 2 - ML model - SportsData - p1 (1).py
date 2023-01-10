c7f2bf50195ba4a71e0e981b452d0d542d6ce0b4
asam1234 <albertsam87@hotmail.com>
2023-01-10 15:09:17 +0000

    Add files via upload

---
+++ Project 2 - ML model - SportsData - p1 (1).py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[39]:
+
+
+# ML Model - this is code for creation of the model. 
+
+import pandas as pd 
+from sklearn.tree import DecisionTreeClassifier
+
+sports_data = pd.read_csv('sportsdata.csv')
+
+# input data
+X = sports_data.drop(columns = 'sport')
+
+# output data
+y = sports_data['sport']
+
+model = DecisionTreeClassifier()
+model.fit(X.values, y.values) 
+predictions = model.predict([[21,1],[42,0]])
+predictions
+
